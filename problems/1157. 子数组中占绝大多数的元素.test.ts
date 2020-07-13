7d1da77f4c9108b94fd64233d45cf4174b4dca3b
haua <hhhaua@gmail.com>
2020-07-13 18:34:30 +0800

    增加测试用例，优化版本

--- problems/1157. 子数组中占绝大多数的元素.test.ts
+++ problems/1157. 子数组中占绝大多数的元素.test.ts
@@ -1,4 +1,4 @@
-import { teal } from 'color-name'
+import stopwatch from '../libs/stopwatch'
 
 const data: {
   init: any
@@ -16,7 +16,10 @@ interface MajorityCheckerInstT {
 
 const check = (MajorityChecker: MajorityCheckerT) => {
   data.forEach(data => {
+    console.log(1212, data.init)
+    stopwatch.mark()
     const inst = new MajorityChecker(data.init)
+    console.log('init耗时：', stopwatch.mark().msg)
     data.params.forEach((d: number[], i) => {
       const res = inst.query(d[0], d[1], d[2])
       if(data.results[i] !== undefined){
@@ -28,10 +31,11 @@ const check = (MajorityChecker: MajorityCheckerT) => {
         expect([-1, 1, 2].includes(res)).toBe(true);
       }
     })
+    console.log('query耗时：', stopwatch.mark().msg)
   })
 }
 
-// 650ms左右
+// 2s左右
 test('常规暴力算法', () => {
   class MajorityChecker {
     readonly array: number[] = []
@@ -74,44 +78,48 @@ test('常规暴力算法', () => {
 
 test('优化算法', () => {
   class MajorityChecker {
-    // 保存每个值在arr的所有index
-    readonly valueIndexs: {[k:string]:number[]} = {}
-    readonly valueReverseIndexs: {[k:string]:number[]} = {}
-
     // 每个值在数组中的index指定的位置出现过多少次
-    readonly itemNums: Map<number, number[]> = new Map<number, number[]>()
+    readonly itemNums: {[k: number]: number[]} = {}
+    readonly items: Set<number> = new Set<number>()
 
-    allNumMap: {[k:string]:number[]} = {}
+    temp: Map<string, number> = new Map<string, number>()
 
     constructor(arr: number[]) {
-      const items: Set<number> = new Set()
-      arr.forEach((a) => {
-        items.add(a)
-      })
+      const items: Set<number> = new Set(arr)
 
-      const map = new Map<number, number[]>()
+      this.items = items
+
+      stopwatch.mark('111')
+
+      const map: {[k: number]: number[]} = {}
       for (const item of items){
-        map.set(item, [])
+        map[item] = new Array(arr.length + 1).fill(0)
       }
 
-      arr.forEach((a) => {
-        for (const [k, v] of map) {
-          v.push(0)
-          map.set(k, v)
-        }
+      console.log(111, stopwatch.mark('111').msg)
 
-        const nums = map.get(a) || [0]
-        nums[nums.length - 1] = nums[nums.length - 2] + 1
-        map.set(a, nums)
+      arr.forEach((a, i) => {
+        for (const item of items) {
+          map[item][i + 1] = map[item][i] + (item === a ? 1 : 0)
+        }
       })
+
+      console.log(222, stopwatch.mark('111').msg)
 
       this.itemNums = map
     }
 
     query(left: number, right: number, threshold: number): number {
-      const {itemNums} = this
-      for (const [item, nums] of itemNums){
+      const {itemNums, temp, items} = this
+
+      const k = `${left}-${right}`
+      if(temp.has(k)){
+        return temp.get(k) || 0
+      }
+      for (const item of items){
+        const nums = itemNums[item]
         if (nums[right + 1] - nums[left] >= threshold) {
+          temp.set(k, item)
           return item
         }
       }
@@ -123,24 +131,16 @@ test('优化算法', () => {
   check(MajorityChecker)
 })
 
+// 810ms，有时会上到1s
 test('大神的算法', () => {
   class MajorityChecker {
     s:number
     a: number[]
     b: number[][]
     N: number
-    d: number[] = new Array(20005).fill(0)
+    d: number[] = []
 
     constructor (arr: number[]) {
-      this.b = new Array(205).fill(0).map(
-        () => new Array(20005).fill(0)
-      )
-      this.d = new Array(20005).fill(0)
-      this.a = arr
-      const n = arr.length
-      this.s = Math.floor(Math.sqrt(n * 2)) // 数组长度两倍的根号二，暂不明白干嘛用，这个比数组长度低
-      this.N = 0
-
       const map = new Map()
       arr.forEach(x => {
         if(map.has(x)) {
@@ -149,6 +149,15 @@ test('大神的算法', () => {
           map.set(x, 1)
         }
       })
+
+      this.b = new Array(Math.min(map.size, 200) + 5).fill(0).map(
+        () => new Array(arr.length + 5).fill(0)
+      )
+      this.d = new Array(arr.length + 5).fill(0)
+      this.a = arr
+      const n = arr.length
+      this.s = Math.floor(Math.sqrt(n * 2)) // 数组长度两倍的根号二，暂不明白干嘛用，这个比数组长度低
+      this.N = 0
 
       // 这不是数组长度的一半，这个比数组长度低很多倍，但至少是1，除以2之后有可能带小数
       const half = this.s / 2
